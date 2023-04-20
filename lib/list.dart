f3e034ad5e3b8ca77fc0a08e681049adeb7805ae
Rahul R <rahulr441989@gmail.com>
2023-04-20 11:51:56 +0530

    CameraApp using ImagePicker

---
+++ lib/list.dart
@@ -0,0 +1,50 @@
+import 'package:camera/imageModel.dart';
+import 'package:flutter/material.dart';
+
+class CameraList extends StatefulWidget {
+  const CameraList({super.key});
+
+  @override
+  State<CameraList> createState() => _CameraListState();
+}
+
+class _CameraListState extends State<CameraList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown.shade200,
+      appBar: AppBar(
+        title: const Text('Gallery'),
+        centerTitle: true,
+        backgroundColor: Colors.tealAccent.shade700,
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: imageListNotifier,
+        builder: (context, value, child) {
+         return ListView.separated(
+          itemBuilder: (context,index){
+            final data1 = value[index];
+            return Container(
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Image.file(data1.img, width: 100, height: 100,),
+                    ),
+                ],
+              ),
+            );
+          }, 
+          separatorBuilder: (context,index){
+            return const Divider();
+          }, 
+          itemCount: value.length
+          ); 
+        }
+      ),
+    );
+  }
+}
+
